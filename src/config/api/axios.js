cf8f8193f37e57bc0e497d941bcd1b36094d0f22
Yabha <ay3581342@gmail.com>
2024-05-03 22:53:26 +0530

    deployment down

--- src/config/api/axios.js
+++ src/config/api/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export default axios.create({
   baseURL:
-    "https://eedusys-backend.onrender.com",
-     //"http://localhost:3500",
+    //"https://eedusys-backend.onrender.com",
+     "http://localhost:3500",
 
   headers: { "Content-Type": "application/json" },
 });
