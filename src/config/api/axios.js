e04c0471864d0b789c24c05aa2444894677ab152
Yabha <ay3581342@gmail.com>
2024-05-03 22:47:41 +0530

    railway removed

--- src/config/api/axios.js
+++ src/config/api/axios.js
@@ -2,9 +2,8 @@ import axios from "axios";
 
 export default axios.create({
   baseURL:
-   // "https://eedusys-backend.onrender.com",
+    "https://eedusys-backend.onrender.com",
      //"http://localhost:3500",
-     "eedusys-backend-production.up.railway.app",
 
   headers: { "Content-Type": "application/json" },
 });
